016929431f28e8e013a63a2aa1ff75e6c60a21ca
Vincent Delaitre <vincent@deepomatic.com>
2019-05-01 18:10:55 +0200

    Fix typo in kube config (#4)

--- release/make_nutanix_manifest.sh
+++ release/make_nutanix_manifest.sh
@@ -4,11 +4,10 @@ set -e
 
 ROOT=$(dirname $0)
 
-export DOCKER_IMAGE_NAME=deepomatic/nutanix-runtime:master-10
+export DOCKER_IMAGE_NAME=deepomatic/nutanix-runtime:master-19
 export DEPLOYMENT_NAME=deepomatic-app
 export DEEPOMATIC_RUN_VERSION=0.1.0
 export DEEPOMATIC_SITE_ID=${NUTANIX_RUNTIME_SITE_ID}
-export GPU_FLAG="{name: GPU_IDS, value: \"0\"}"
 export IMAGE_PULL_SECRETS=""
 export NATS_SERVICE=""
 
