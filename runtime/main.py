f108443101071c27baa956cf745a2d37cdd84c12
Vincent Delaitre <vincent@deepomatic.com>
2019-05-01 14:48:47 +0200

    Refactor to wrap everything into a MessageHandler class

--- runtime/main.py
+++ runtime/main.py
@@ -1,13 +1,14 @@
 import os
+import sys
 import asyncio
 import logging
+import textwrap
 import io
 from PIL import Image, ImageFont, ImageDraw
 from google.protobuf.json_format import MessageToDict
 
 import xi_iot_pb2
 from nats.aio.client import Client as NATS
-#from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 
 from deepomatic.rpc.client import Client
 from deepomatic.rpc import v07_ImageInput
@@ -15,100 +16,174 @@ from deepomatic.rpc.helpers.v07_proto import create_images_input_mix, create_wor
 
 logger = logging.getLogger(__name__)
 
-# Configure worker queue
-command_queue_name = 'worker_queue'
-amqp_url = os.getenv('AMQP_URL')
+# --------------------------------------------------------------------------- #
 
-# configure drawing font
-font = ImageFont.truetype(os.path.join(os.path.dirname(__file__), "arial.ttf"), 48)
+class Config(object):
+    """
+    Stores global configuration
+    """
 
-# Setup global variables
-client = Client(amqp_url)
-command_queue = client.new_queue(command_queue_name)
-response_queue, consumer = client.new_consuming_queue()
+    def __init__(self):
+        # Should we draw a label or just pass inference results ?
+        self.draw_demo = os.getenv('DRAW_DEMO') == "1"
 
-nc = None
-nats_broker_url = ""
-src_nats_topic = ""
-dst_nats_topic = ""
+        # configure drawing font
+        self.font = ImageFont.truetype(os.path.join(os.path.dirname(__file__), "arial.ttf"), 48)
+        self.font_color = self.get_font_color()
 
-def get_nats_meta():
-    global nats_broker_url, src_nats_topic, dst_nats_topic
-    nats_broker_url = os.environ.get('NATS_ENDPOINT')
-    if nats_broker_url is None:
-        logger.error('nats broker not provided in environment var NATS_ENDPOINT')
-        exit(1)
+        # Setup deepomatic Run RPC client
+        amqp_url = os.getenv('AMQP_URL')
+        if amqp_url is None:
+            raise Exception('AMQP broker not provided in environment var AMQP_URL')
+        self.command_queue_name = os.getenv('WORKER_QUEUE', 'worker_queue')
+        self.amqp_client = Client(amqp_url)
+        self.amqp_client.new_queue(self.command_queue_name)
+        self.amqp_response_queue, self.amqp_consumer = self.amqp_client.new_consuming_queue()
 
-    src_nats_topic = os.environ.get('NATS_SRC_TOPIC')
-    if src_nats_topic is None:
-        logger.error('src nats topic not provided in environment var NATS_SRC_TOPIC')
-        exit(1)
-    dst_nats_topic = os.environ.get('NATS_DST_TOPIC')
+        # Setup Nutanix NATS client
+        self.nats_client = NATS()
+        self.nats_broker_url = os.environ.get('NATS_ENDPOINT')
+        if self.nats_broker_url is None:
+            raise Exception('nats broker not provided in environment var NATS_ENDPOINT')
 
-    if dst_nats_topic is None:
-        logger.error('dst nats broker not provided in environment var NATS_DST_TOPIC')
-        exit(1)
-    return nats_broker_url, src_nats_topic, dst_nats_topic
+        self.nats_src_topic = os.environ.get('NATS_SRC_TOPIC')
+        if self.nats_src_topic is None:
+            raise Exception('src nats topic not provided in environment var NATS_SRC_TOPIC')
 
-async def message_handler(msg):
-    try:
-        logger.info("Received a message!")
+        self.nats_dst_topic = os.environ.get('NATS_DST_TOPIC')
+        if self.nats_dst_topic is None:
+            raise Exception('dst nats broker not provided in environment var NATS_DST_TOPIC')
+
+        logger.info("broker: {b}, src topic: {s}, dst_topic: {d}".format(
+            b=self.nats_broker_url,
+            s=self.nats_src_topic,
+            d=self.nats_dst_topic))
+
+    @staticmethod
+    def get_font_color():
+        """
+        Convert environment variable FONT_COLOR from RRGGBB or RGB in hexadecimal format
+        to a tuple (R, G, B).
+        """
+        color = os.getenv('FONT_COLOR', 'FFFFFF')
+        try:
+            if len(color) == 6:
+                return tuple([int(c, 16) for c in textwrap.wrap(color, 2)])
+            elif len(color) == 3:
+                return tuple([int(c, 16) * 17 for c in color])
+        except ValueError:
+            pass
+        logger.error("Error parsing font color, defaulting to white")
+        return (255, 255, 255)
+
+# --------------------------------------------------------------------------- #
+
+class MessageHandler(object):
+
+    def __init__(self, config):
+        self._config = config
+
+    @staticmethod
+    def image_from_message(msg):
+        """
+        Convert input payload into an image
+        """
         _msg = xi_iot_pb2.DataStreamMessage()
         _msg.ParseFromString(msg.data)
+        return _msg.payload
 
+    @staticmethod
+    def message_from_image(image):
+        """
+        Convert image into output payload
+        """
+        msg = xi_iot_pb2.DataStreamMessage()
+        msg.payload = image
+        return msg.SerializeToString()
+
+    def send_inference_request(self, image):
+        """
+        Send an inference command to the neural worker and wait for the result
+        """
         # Create a recognition command mix
         command = create_workflow_command_mix()
 
         # This assumes a mono-input network
-        image_input = v07_ImageInput(source=b'data:image/*;binary,' + _msg.payload)
+        image_input = v07_ImageInput(source=b'data:image/*;binary,' + image)
         inputs = create_images_input_mix([image_input])
 
         # Send the request
         logger.info("Sending inference request to worker")
-        correlation_id = client.command(command_queue_name, response_queue.name, inputs, command)
+        correlation_id = self._config.amqp_client.command(
+            self._config.command_queue_name,
+            self._config.amqp_response_queue.name, inputs, command)
 
         # Wait for response, `timeout=float('inf')` or `timeout=-1` for infinite wait, `timeout=None` for non blocking
-        response = consumer.get(correlation_id, timeout=-1)
+        response = self._config.amqp_consumer.get(correlation_id, timeout=-1)
         # Put data as returned by Deepomatic API v0.7
         data = {'outputs': [MessageToDict(o, preserving_proto_field_name=True, including_default_value_fields=True) for o in response.to_parsed_result_buffer()]}
         logger.info("Got inference response: {}".format(data))
+        return data
 
-        # Drawn label on image
-        label = data['outputs'][0]['labels']['predicted'][0]['label_name']
-        img = Image.open(io.BytesIO(_msg.payload))
+    def draw_label(self, image, inference_result):
+        """
+        Draw infered label in top-left corner of the image
+        """
+        label = inference_result['outputs'][0]['labels']['predicted'][0]['label_name']
+        img = Image.open(io.BytesIO(image))
         draw = ImageDraw.Draw(img)
-        draw.text((10, 10), label, (255, 255, 255), font=font)
+        draw.text((10, 10), label, self._config.font_color, font=self._config.font)
         with io.BytesIO() as buff:
             img.save(buff, format="JPEG")
-            _msg = xi_iot_pb2.DataStreamMessage()
-            _msg.payload = buff.getvalue()
+            return buff.getvalue()
 
-        # RFC: We could leverage `reply` topic as the destination topic which would not require DST_NATS_TOPIC to be provided
+    async def message_handler(self, msg):
+        try:
+            logger.info("Received a message!")
+            image = self.image_from_message(msg)
+
+            # Perform inference
+            inference_result = self.send_inference_request(image)
+
+            # Draw label on the image
+            image = self.draw_label(image, inference_result)
+
+            # Send the result image
+            await self.send_result(self.message_from_image(image))
+
+        except Exception as e:
+            # Catch an display errors which are otherwise not shown
+            logger.error("{}".format(e))
+
+    async def send_result(self, msg):
+        # RFC: We could leverage `reply` topic as the destination topic which would not require NATS_DST_TOPIC to be provided
         # await nc.publish(reply, data)
-        await nc.publish(dst_nats_topic, _msg.SerializeToString())
-    except Exception as e:
-        logger.error("{}".format(e))
+        await self._config.nats_client.publish(self._config.nats_dst_topic, msg)
 
-async def run(loop):
-    nats_broker_url, src_nats_topic, dst_nats_topic = get_nats_meta()
-    logger.info("broker: {b}, src topic: {s}, dst_topic: {d}".format(b=nats_broker_url, s=src_nats_topic, d=dst_nats_topic))
+    async def connect(self, loop):
+        try:
+            # This will return immediately if the server is not listening on the given URL
+            await self._config.nats_client.connect(self._config.nats_broker_url, loop=loop)
+            logger.info("Connected to broker")
 
-    global nc
-    nc = NATS()
+            await self._config.nats_client.subscribe(self._config.nats_src_topic, cb=self.message_handler)
+        except Exception as e:
+            # Catch an display errors which are otherwise not shown
+            logger.error("{}".format(e))
 
-    # This will return immediately if the server is not listening on the given URL
-    await nc.connect(nats_broker_url, loop=loop)
-    logger.info("Connected to broker")
+    def run(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.connect(loop))
+        try:
+            loop.run_forever()
+        finally:
+            self._config.nats_client.drain()
+            loop.close()
 
-    await nc.subscribe(src_nats_topic, cb=message_handler)
+
+# --------------------------------------------------------------------------- #
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(loop))
-    loop.run_forever()
-    try:
-        loop.run_until_complete(run(loop))
-        loop.run_forever()
-    finally:
-        nc.drain()
-        loop.close()
+    config = Config()
+    handler = MessageHandler(config)
+    handler.run()
