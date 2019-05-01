00ee302e6d011b5120cc99333b390fbbb5ca15b4
Vincent Delaitre <vincent@deepomatic.com>
2019-05-01 19:14:38 +0200

    Fix default color

--- release/make_nutanix_manifest.sh
+++ release/make_nutanix_manifest.sh
@@ -4,7 +4,7 @@ set -e
 
 ROOT=$(dirname $0)
 
-export DOCKER_IMAGE_NAME=deepomatic/nutanix-runtime:master-19
+export DOCKER_IMAGE_NAME=deepomatic/nutanix-runtime:master-22
 export DEPLOYMENT_NAME=deepomatic-app
 export DEEPOMATIC_RUN_VERSION=0.1.0
 export DEEPOMATIC_SITE_ID=${NUTANIX_RUNTIME_SITE_ID}
