b5c4e019964a4c3c34aae1145246a52366680763
Vincent Delaitre <vincent@deepomatic.com>
2019-05-01 22:05:35 +0200

    Use rc versions of services

--- release/make_nutanix_manifest.sh
+++ release/make_nutanix_manifest.sh
@@ -6,7 +6,8 @@ ROOT=$(dirname $0)
 
 export DOCKER_IMAGE_NAME=deepomatic/nutanix-runtime:master-22
 export DEPLOYMENT_NAME=deepomatic-app
-export DEEPOMATIC_RUN_VERSION=0.1.0
+export NEURAL_WORKER_VERSION=deepomatic/vulcan-worker-nn-on-premises-rc:master-495-combo
+export RESOURCE_SERVER_VERSION=deepomatic/resource-server-on-premises-rc:master2-2
 export DEEPOMATIC_SITE_ID=${NUTANIX_RUNTIME_SITE_ID}
 export IMAGE_PULL_SECRETS=""
 export NATS_SERVICE=""
