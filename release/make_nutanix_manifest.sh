615be01cf65dd863a695d73a269daab5896f88ce
Vincent Delaitre <vincent@deepomatic.com>
2019-05-04 02:31:36 +0200

    Add volume claim

--- release/make_nutanix_manifest.sh
+++ release/make_nutanix_manifest.sh
@@ -2,19 +2,18 @@
 
 set -e
 
-ROOT=$(dirname $0)
+TEMPLATE=$1
 
-export DOCKER_IMAGE_NAME=deepomatic/nutanix-runtime:master-22
-export DEPLOYMENT_NAME=deepomatic-app
+export DOCKER_IMAGE_NAME=deepomatic/nutanix-app:0.1.0
 export NEURAL_WORKER_VERSION=deepomatic/run-neural-worker:0.2.0-rc.1
 export RESOURCE_SERVER_VERSION=deepomatic/run-resource-server:0.2.0-rc.1
+export DEPLOYMENT_NAME=deepomatic-app
+export STORAGE_CLASS=silver
 export DEEPOMATIC_APP_ID=${NUTANIX_RUNTIME_APP_ID}
 export DEEPOMATIC_API_KEY=${NUTANIX_RUNTIME_API_KEY}
 export DEEPOMATIC_SITE_ID=${NUTANIX_RUNTIME_SITE_ID}
-export IMAGE_PULL_SECRETS=""
-export NATS_SERVICE=""
 
 eval "cat <<EOF
-$(<${ROOT}/../deploy/k8s-manifest.yml)
+$(<${TEMPLATE})
 EOF
-" 2> /dev/null > ${ROOT}/nutanix-manifest.yml
+" 2> /dev/null > manifest.yml
